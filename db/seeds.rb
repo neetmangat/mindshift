604153cd05a0a92bc0bd589080e6ce7ac8aa09a3
neetmangat <neetmangat@gmail.com>
2020-06-18 08:01:40 +0000

    seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!([
+  {id: 1, email: "test@test.com", password: "password", reset_password_token: "0bda2b5099a3a8701903dc07dc3c168317e0d46270a5a10f752bbed6b70709a8", reset_password_sent_at: "2020-06-09 01:12:24", remember_created_at: nil},
+  {id: 2, email: "neetmangat@gmail.com", password: "password", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil}
+])
+
+Course.create!([
+  {id: 12, title: "How to Code Trading Bots", description: "Learn how to code bots that will trade stocks, crypto, and forex for you based on strategies you come up with!", cost: "149.0", user_id: 2, image: "coding.jpg"},
+  {id: 13, title: "Finance 101", description: "Learn about finance, investing, and retire early.", cost: "99.0", user_id: 1, image: "finance.jpg"},
+  {id: 14, title: "Introduction to Options", description: "Options trading is a powerful way to limit risk while amplifying rewards. This course teaches you the basics of options terminology and explores various strategies used by professionals. No prior experience is needed.", cost: "149.0", user_id: 1, image: "stock_options_svu3hu.jpeg"}
+])
+
+Section.create!([
+  {id: 8, title: "Week 1", course_id: 13},
+  {id: 9, title: "Week 2", course_id: 13},
+  {id: 10, title: "Beginner", course_id: 14},
+  {id: 11, title: "Intermediate", course_id: 14},
+  {id: 12, title: "Expert", course_id: 14}
+])
+
+Lesson.create!([
+  {id: 7, title: "Why Finance Matters", subtitle: "Money makes the world go round.", section_id: 8},
+  {id: 8, title: "Saving or Investing?", subtitle: "Explore the power of capitalism.", section_id: 9},
+  {id: 9, title: "Basic Terminology", subtitle: "Calls, Puts, Writing, Spreads", section_id: 10},
+  {id: 10, title: "Risk vs. Reward", subtitle: "How to minimize risk & maximize reward.", section_id: 10},
+  {id: 11, title: "Spreads", subtitle: "Indecisive? No problem.", section_id: 11},
+  {id: 12, title: "Tested Strategies", subtitle: "Try Before You Buy", section_id: 12}
+])
