45855a88e9f6f1d18c8c4775c69fe7ce00a644d4
neetmangat <neetmangat@gmail.com>
2020-06-18 23:41:42 +0000

    HTTPS vs HTTP Heroku

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,7 +1,8 @@
 class CoursesController < ApplicationController
 
     def index
-        @courses = Course.all.order(created_at: :desc)
+        @courses = Course.all
+        #@courses = Course.all.order(created_at: :desc)
     end
 
     def show
