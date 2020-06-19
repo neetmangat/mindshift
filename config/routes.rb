3ba4a529a1d6ba68ffda227837df364da77b9dfe
neetmangat <neetmangat@gmail.com>
2020-06-19 08:44:51 +0000

    Enrollment Setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root 'static_pages#index'
-  resources :courses, only: [:index, :show]
+
+  resources :courses, only: [:index, :show] do
+    resources :enrollments, only: :create
+  end
+
   resources :lessons, only: [:show]
+
   namespace :instructor do
     resources :sections, only: [] do
         resources :lessons, only: [:new, :create]
@@ -11,4 +17,5 @@ Rails.application.routes.draw do
       resources :sections, only: [:new, :create]
     end
   end
+
 end
